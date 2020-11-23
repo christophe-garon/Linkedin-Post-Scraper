01f66fd9f0dcad099e5db650139dce9b0a3f12a4
christophe-garon <69330244+christophe-garon@users.noreply.github.com>
2020-11-23 09:58:43 -0600

    Fixed popup promotion bug

--- linkedin_scraper_public.py
+++ linkedin_scraper_public.py
@@ -16,7 +16,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 import time
 import pandas as pd
-import re as re
+
 
 
 #accessing Chromedriver
@@ -24,12 +24,11 @@ browser = webdriver.Chrome('chromedriver')
 
 
 #Replace with you username and password
-username = "linkedin_username"
-password = "linkedin password"
+username = "username"
+password = "password"
 
 #Open login page
 browser.get('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
-
 
 #Enter login info:
 elementID = browser.find_element_by_id('username')
@@ -40,8 +39,8 @@ elementID.send_keys(password)
 elementID.submit()
 
 
-#Go to company webpage. Change page as needed
-browser.get('https://www.linkedin.com/company/lor%C3%A9al/')
+#Go to company webpage
+browser.get('https://www.linkedin.com/company/johnson-johnson-consumer-health/')
 
 
 #Simulate scrolling to capture all posts
@@ -92,68 +91,104 @@ containers = linkedin_soup.findAll("div",{"class":"occludable-update ember-view"
 container = containers[0].find("div","feed-shared-update-v2__description-wrapper ember-view")
 
 
-
 # Lists that we will iterate to
 post_dates = []
 post_texts = []
 post_likes = []
 post_comments = []
 video_views = []
+media_links = []
+media_type = []
 
 
 #Looping through the posts and appending them to the lists
 for container in containers:
-    posted_container = container.findAll("div",{"class": "display-flex feed-shared-actor display-flex feed-shared-actor--with-control-menu ember-view"})
-    posted_date = posted_container[0].find("span",{"class": "feed-shared-actor__sub-description t-12 t-normal t-black--light"})
-    box_container = container.findAll("div",{"class": "feed-shared-update-v2__description-wrapper ember-view"})
-    new_box = box_container[0].find("span",{"dir":"ltr"})
-    engagement_container = container.findAll("ul", class_="social-details-social-counts ember-view")
-    new_likes = engagement_container[0].find("span", {"class": "v-align-middle social-details-social-counts__reactions-count"})
-    new_comments = engagement_container[0].find("li", {"class": "social-details-social-counts__comments social-details-social-counts__item"})
+    
+    try:
+        posted_date = container.find("span",{"class":"visually-hidden"})
+        text_box = container.find("div",{"class":"feed-shared-update-v2__description-wrapper ember-view"})
+        text = text_box.find("span",{"dir":"ltr"})
+        new_likes = container.findAll("li", {"class":"social-details-social-counts__reactions social-details-social-counts__item"})
+        new_comments = container.findAll("li", {"class": "social-details-social-counts__comments social-details-social-counts__item"})
 
-    #Getting Video Views. (The folling three lines prevents class name overlap)
-    view_container2 = set(container.findAll("li", {'class':["social-details-social-counts__item","social-details-social-counts__reactions social-details-social-counts__item"]}))
-    view_container1 = set(container.findAll("li", {'class':["social-details-social-counts__reactions","social-details-social-counts__comments social-details-social-counts__item"]}))
-    result = view_container2 - view_container1
-    
-    #Upacking the set into list
-    view_container = []
-    for i in result:
-        view_container += i
-    
-    
-    #Adding post views, likes, and comment values to lists if they exists
-    try:
-        video_views.append(view_container[1].text.strip().replace(' Views',''))
+        #Appending date and text to lists
+        post_dates.append(posted_date.text.strip())
+        post_texts.append(text_box.text.strip())
+
+
+        #Determining media type and collecting video views if applicable
+        try:
+            video_box = container.findAll("div",{"class": "feed-shared-update-v2__content feed-shared-linkedin-video ember-view"})
+            video_link = video_box[0].find("video", {"class":"vjs-tech"})
+            media_links.append(video_link['src'])
+            media_type.append("Video")
+        except:
+            try:
+                image_box = container.findAll("div",{"class": "feed-shared-image__container"})
+                image_link = image_box[0].find("img", {"class":"ivm-view-attr__img--centered feed-shared-image__image feed-shared-image__image--constrained lazy-image ember-view"})
+                media_links.append(image_link['src'])
+                media_type.append("Image")
+            except:
+                try:
+                    image_box = container.findAll("div",{"class": "feed-shared-image__container"})
+                    image_link = image_box[0].find("img", {"class":"ivm-view-attr__img--centered feed-shared-image__image lazy-image ember-view"})
+                    media_links.append(image_link['src'])
+                    media_type.append("Image")
+                except:
+                    try:
+                        article_box = container.findAll("div",{"class": "feed-shared-article__link-container"})
+                        article_link = article_box[0].find("a", {"class":"feed-shared-article__image-link tap-target app-aware-link ember-view"})
+                        media_links.append(article_link['href'])
+                        media_type.append("Article")
+                    except:
+                        try:
+                            poll_box = container.findAll("div",{"class": "feed-shared-update-v2__content overflow-hidden feed-shared-poll ember-view"})
+                            media_links.append("None")
+                            media_type.append("Other (Shared Post/Poll, etc.)")
+                        except:
+                            media_links.append("None")
+                            media_type.append("Unknown")
+
+
+
+        #Getting Video Views. (The folling three lines prevents class name overlap)
+        view_container2 = set(container.findAll("li", {'class':["social-details-social-counts__item"]}))
+        view_container1 = set(container.findAll("li", {'class':["social-details-social-counts__reactions","social-details-social-counts__comments social-details-social-counts__item"]}))
+        result = view_container2 - view_container1
+
+        view_container = []
+        for i in result:
+            view_container += i
+
+        try:
+            video_views.append(view_container[1].text.strip().replace(' Views',''))
+
+        except:
+            video_views.append('N/A')
+
+        
+        #Appending likes and comments if they exist
+        try:
+            post_likes.append(new_likes[0].text.strip())
+        except:
+            post_likes.append(0)
+            pass
+
+        try:
+            post_comments.append(new_comments[0].text.strip())                           
+        except:                                                           
+            post_comments.append(0)
+            pass
     
     except:
-        video_views.append('')
-    
-    
-    try:
-        post_likes.append(new_likes.text.strip())
-    except:
-        post_likes.append(0)
         pass
 
     
-    try:
-        post_comments.append(new_comments.text.strip())                           
-    except:                                                           
-        post_comments.append(0)
-        pass
-    
-    #Adding post date and text to list
-    post_dates.append(posted_date.text.strip())
-    post_texts.append(new_box.text.strip())
-
-
-    
-#Cleaning the dates
-cleaned_dates = []
-for i in post_dates:
-    d = str(i[0:3]).replace('\n\n', '').replace('•','').replace(' ', '')
-    cleaned_dates += [d]
+# #Cleaning the dates
+# cleaned_dates = []
+# for i in post_dates:
+#     d = str(i[0:3]).replace('\n\n', '').replace('•','').replace(' ', '')
+#     cleaned_dates += [d]
 
 
 #Stripping non-numeric values
@@ -166,11 +201,13 @@ for i in post_comments:
     
 #Constructing Pandas Dataframe
 data = {
-    "Date Posted": cleaned_dates,
+    "Date Posted": post_dates,
+    "Media Type": media_type,
     "Post Text": post_texts,
     "Post Likes": post_likes,
     "Post Comments": comment_count,
-    "Video Views": video_views
+    "Video Views": video_views,
+    "Media Links": media_links
 }
 
 df = pd.DataFrame(data)
